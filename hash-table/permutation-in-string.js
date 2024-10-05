4ae13b05f097fd59a9ab4ccb434c4e87be5253b4
Dinesh Chaudhari <dinesh.chaudhari0310@gmail.com>
2024-10-05 12:45:28 +0530

    LC-permutation-in-string [Runtime: 82 ms; Memory: 52.2 MB]

--- hash-table/permutation-in-string.js
+++ hash-table/permutation-in-string.js
@@ -4,19 +4,29 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-    const reverseS1 = s1.trim().split("").reverse().join("");
+    const len1 = s1.length, len2 = s2.length;
+    if (len1 > len2) return false;
 
-    let index = 0;
-    for(let i = 0; i < s2.length; i++){
-        if(index == reverseS1.length) return true;
-
-        if(s2[i] == reverseS1[index]){
-            index++;
-        }else{
-            index = 0;
-        }
+    let neededChar = {};
+    for (let i = 0; i < s1.length; i++) {
+        neededChar[s1[i]] = (neededChar[s1[i]] || 0) + 1;
     }
 
-    if(index == reverseS1.length) return true;
+    let left = 0, //left pointer/index of the sliding window
+		right = 0, //right pointer/index of the sliding window
+		requiredLength = s1.length;
+
+    while(right < s2.length) {
+        if (neededChar[s2[right]] > 0) requiredLength--;
+        neededChar[s2[right]]--;
+        right++;
+
+        if (requiredLength === 0) return true;
+        if (right - left === s1.length) {
+            if (neededChar[s2[left]] >= 0) requiredLength++;
+            neededChar[s2[left]]++;
+            left++
+        }
+    }
     return false;
 };
