432e670ec6c207bbb2ef144c74e7e9fa41870e7f
Dinesh Chaudhari <dinesh.chaudhari0310@gmail.com>
2024-10-06 20:41:24 +0530

    LC-minimum-remove-to-make-valid-parentheses [Runtime: 71 ms; Memory: 55.5 MB]

--- stack/minimum-remove-to-make-valid-parentheses.js
+++ stack/minimum-remove-to-make-valid-parentheses.js
@@ -3,40 +3,21 @@
  * @return {string}
  */
 var minRemoveToMakeValid = function(s) {
-    let validParenthese = "";
     let stack = [];
-    let start = -1, end = -1;;
+    let splittedStr = s.split("");
+
     for(let i = 0; i < s.length; i++){
         if(s[i] == '('){
-            stack.push({
-                ele: '(',
-                i
-            });
-        }else if(s[i] == ')' && stack[stack.length-1] && stack[stack.length-1].ele == '('){
-            if(start == -1) start = stack[stack.length-1].i;
-            if(end == -1) end = i;
+            stack.push(i);
+        }else if(s[i] == ')'){
+            if(stack.length == 0) splittedStr[i] = "";
+            else stack.pop();
+        }
+    }
 
-            if(stack[stack.length-1].i < start){
-                validParenthese = '(' + validParenthese + ')';
-                start = stack[stack.length-1].i
-            }else{
-                validParenthese = validParenthese + '()';
-            }
-            stack.pop();
-        }
+    //remove extra '(' bracket from splittedStr
+    for(let i = 0; i < stack.length; i++){
+        splittedStr[stack[i]] = "";
     }
-    console.log("validParenthese: ",validParenthese)
-    let index = 0;
-    let ans = "";
-    for(let i = 0; i < s.length; i++){
-        if(s[i] == '(' || s[i] == ')'){
-            if(s[i] == validParenthese[index]){
-                ans += s[i];
-                index++;
-            }
-        }else{
-            ans += s[i];
-        }
-    }
-    return ans;
+    return splittedStr.join("");
 };
