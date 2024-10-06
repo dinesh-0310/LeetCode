86f5f9f45df0efe7b01832c33df7f41bc839b0ac
Dinesh Chaudhari <dinesh.chaudhari0310@gmail.com>
2024-10-06 20:30:14 +0530

    LC-minimum-remove-to-make-valid-parentheses [Runtime: N/A; Memory: N/A]

--- string/minimum-remove-to-make-valid-parentheses.js
+++ string/minimum-remove-to-make-valid-parentheses.js
@@ -5,14 +5,27 @@
 var minRemoveToMakeValid = function(s) {
     let validParenthese = "";
     let stack = [];
+    let start = -1, end = -1;;
     for(let i = 0; i < s.length; i++){
         if(s[i] == '('){
-            stack.push(s[i]);
-        }else if(s[i] == ')' && stack[stack.length-1] == '('){
-            validParenthese = '(' + validParenthese + ')';
+            stack.push({
+                ele: '(',
+                i
+            });
+        }else if(s[i] == ')' && stack[stack.length-1] && stack[stack.length-1].ele == '('){
+            if(start == -1) start = stack[stack.length-1].i;
+            if(end == -1) end = i;
+
+            if(stack[stack.length-1].i < start){
+                validParenthese = '(' + validParenthese + ')';
+                start = stack[stack.length-1].i
+            }else{
+                validParenthese = validParenthese + '()';
+            }
             stack.pop();
         }
     }
+    console.log("validParenthese: ",validParenthese)
     let index = 0;
     let ans = "";
     for(let i = 0; i < s.length; i++){
