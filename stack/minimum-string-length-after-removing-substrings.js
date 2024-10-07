79232c45f7c4a8142e21b4ff8a2f5c56c05446cc
Dinesh Chaudhari <dinesh.chaudhari0310@gmail.com>
2024-10-07 17:19:58 +0530

    LC-minimum-string-length-after-removing-substrings [Runtime: 97 ms; Memory: 53.2 MB]

--- stack/minimum-string-length-after-removing-substrings.js
+++ stack/minimum-string-length-after-removing-substrings.js
@@ -7,29 +7,14 @@ var minLength = function(s) {
     const stack = [];
 
     for(let i = 0; i < arr.length; i++){
-        if(arr[i] != 'B' && arr[i] != 'D'){
-            stack.push({
-                ele: arr[i],
-                i
-            })
+        if(arr[i] == 'B' && stack[stack.length-1] == 'A'){
+            stack.pop();
+        }else if(arr[i] == 'D' && stack[stack.length-1] == 'C'){
+            stack.pop();
         }else{
-            if(arr[i] == 'B' && stack[stack.length - 1] && stack[stack.length - 1].ele == 'A'){
-                arr[i] = "";
-                arr[stack[stack.length - 1].i] = "";
-                stack.pop();
-            }else if(arr[i] == 'D' && stack[stack.length - 1] && stack[stack.length - 1].ele == 'C'){
-                arr[i] = "";
-                arr[stack[stack.length - 1].i] = "";
-                stack.pop();
-            }else{
-                stack.push({
-                    ele: arr[i],
-                    i
-                })
-            }
+            stack.push(arr[i]);
         }
     }
-    //console.log("arr", arr);
-    const str = arr.join("");
-    return str.length;
+    return stack.length;
+ 
 };
