d65ff19d31751c8b905ff4a613f30f7092c9a4de
Dinesh Chaudhari <dinesh.chaudhari0310@gmail.com>
2024-10-07 17:15:59 +0530

    LC-minimum-string-length-after-removing-substrings [Runtime: N/A; Memory: N/A]

---
+++ string/minimum-string-length-after-removing-substrings.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minLength = function(s) {
+    const arr = s.split("");
+    const stack = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] != 'B' && arr[i] != 'D'){
+            stack.push({
+                ele: arr[i],
+                i
+            })
+        }else{
+            if(arr[i] == 'B' && stack[stack.length - 1].ele == 'A'){
+                arr[i] = "";
+                arr[stack[stack.length - 1].i] = "";
+                stack.pop();
+            }else if(arr[i] == 'D' && stack[stack.length - 1].ele == 'C'){
+                arr[i] = "";
+                arr[stack[stack.length - 1].i] = "";
+                stack.pop();
+            }else{
+                stack.push({
+                    ele: arr[i],
+                    i
+                })
+            }
+        }
+    }
+    //console.log("arr", arr);
+    const str = arr.join("");
+    return str.length;
+};
