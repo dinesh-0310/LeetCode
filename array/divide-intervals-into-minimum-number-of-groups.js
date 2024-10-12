1f4f8128a54b7819304daacca69400817cfc2c5e
Dinesh Chaudhari <dinesh.chaudhari0310@gmail.com>
2024-10-12 23:47:41 +0530

    LC-divide-intervals-into-minimum-number-of-groups [Runtime: 429 ms; Memory: 108.7 MB]

--- array/divide-intervals-into-minimum-number-of-groups.js
+++ array/divide-intervals-into-minimum-number-of-groups.js
@@ -3,28 +3,16 @@
  * @return {number}
  */
 var minGroups = function(intervals) {
-    if(intervals.length == 0) return 0;
-    let groups = 0;
-    const sortedIntervals = intervals.sort((a, b) => a[0] - b[0]);
-    groups++;
-    //console.log("sortedIntervals: ",sortedIntervals)
-    const obj = {};
-    obj[groups] = sortedIntervals[0];
-    for(let i = 1; i < sortedIntervals.length; i++){
-        let isPartOfGroup = false;
-        for(let key in obj){
-            if(sortedIntervals[i][0] > obj[key][1]){
-                obj[key] = [obj[key][0], sortedIntervals[i][1]];
-                isPartOfGroup = true;
-                break;
-            }
-        }
-        if(!isPartOfGroup){
-            groups++;
-            obj[groups] = sortedIntervals[i];
-        }
-        //console.log("obj & groups: ", obj, groups);
+    const events = [];
+    for (const [start, end] of intervals) {
+        events.push([start, 1]);
+        events.push([end + 1, -1]);
     }
-
-    return groups;
+    events.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
+    let maxGroups = 0, currentGroups = 0;
+    for (const [time, type] of events) {
+        currentGroups += type;
+        maxGroups = Math.max(maxGroups, currentGroups);
+    }
+    return maxGroups;
 };
