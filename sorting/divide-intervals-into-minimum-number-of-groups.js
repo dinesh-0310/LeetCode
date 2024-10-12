b6a88caf92f706ec0460d67f13bba3680e649988
Dinesh Chaudhari <dinesh.chaudhari0310@gmail.com>
2024-10-13 00:10:23 +0530

    LC-divide-intervals-into-minimum-number-of-groups [Runtime: 289 ms; Memory: 84 MB]

--- sorting/divide-intervals-into-minimum-number-of-groups.js
+++ sorting/divide-intervals-into-minimum-number-of-groups.js
@@ -3,16 +3,29 @@
  * @return {number}
  */
 var minGroups = function(intervals) {
-    const events = [];
-    for (const [start, end] of intervals) {
-        events.push([start, 1]);
-        events.push([end + 1, -1]);
+    const starts = [];
+    const ends = [];
+
+    for(let i = 0; i < intervals.length; i++){
+        starts.push(intervals[i][0]);
+        ends.push(intervals[i][1]);
+    };
+    starts.sort((a,b) => a - b);
+    ends.sort((a,b) => a - b);
+
+    let pointer1 = 0;
+    let pointer2 = 0;
+    let res = 0;
+    let groups = 0;
+    while(pointer1 < intervals.length){
+        if(starts[pointer1] <= ends[pointer2]){
+            groups++;
+            pointer1++;
+        }else{
+            groups--;
+            pointer2++;
+        }
+        res = Math.max(res, groups);
     }
-    events.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
-    let maxGroups = 0, currentGroups = 0;
-    for (const [time, type] of events) {
-        currentGroups += type;
-        maxGroups = Math.max(maxGroups, currentGroups);
-    }
-    return maxGroups;
+    return res;
 };
