da85084c257d1d6838c97117546a7c8bf5eaf66d
Dinesh Chaudhari <dinesh.chaudhari0310@gmail.com>
2024-10-14 21:18:24 +0530

    LC-maximal-score-after-applying-k-operations [Runtime: 199 ms; Memory: 69 MB]

--- heap-priority-queue/maximal-score-after-applying-k-operations.js
+++ heap-priority-queue/maximal-score-after-applying-k-operations.js
@@ -4,24 +4,16 @@
  * @return {number}
  */
 var maxKelements = function(nums, k) {
-    let sortedNums = nums.sort((a, b) => b - a);
-    let score = 0;
-    while(k > 0){
-        score += sortedNums[0];
-        let newNum = Math.ceil(sortedNums[0] / 3);
-        sortedNums.shift();
-        let arr = [];
-        sortedNums.forEach((ele, i) =>{
-            if(ele > newNum){
-                arr.push(ele);
-            }else{
-                arr.push(newNum);
-                arr = [...arr, ...sortedNums.filter((e, index) => index > i)];
-                return;
-            }
-        });
-        sortedNums = [...arr];
-        k--;
-    }
-    return score;
+   const priorityQueue = new MaxPriorityQueue({compare: (a, b) => b - a});
+   for(const num of nums){
+    priorityQueue.enqueue(num);
+   }
+   let score = 0;
+   while(k > 0){
+    const ele = priorityQueue.dequeue();
+    score += ele;
+    priorityQueue.enqueue(Math.ceil(ele / 3));
+    k--;
+   }
+   return score;
 };
