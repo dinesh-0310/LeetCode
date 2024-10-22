100154fa5921c584a4b4acf9b7cab13c7a1c3e03
Dinesh Chaudhari <dinesh.chaudhari0310@gmail.com>
2024-10-22 21:51:31 +0530

    LC-kth-largest-sum-in-a-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ binary-tree/kth-largest-sum-in-a-binary-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthLargestLevelSum = function(root, k) {
+    if(root == null) return 0;
+    let obj = {};
+
+    updateObject(root, obj, 0);
+    //console.log("obj: ",obj)
+    if(k >= Object.keys(obj).length) return -1;
+    const values = Object.values(obj).sort((a,b) => b - a);
+    return values[k-1];
+};
+
+function updateObject(root, obj, key){
+    if(!root) return;
+    obj[key] = (obj[key] || 0) + root.val;
+
+    updateObject(root.left, obj, key+1);
+    updateObject(root.right, obj, key+1);
+
+}
